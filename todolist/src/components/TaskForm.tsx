c6af2defb2314f3f49cdb4ae603939775969b3df
Jonatas da  Mata <jonatasdamata1@hotmail.com>
2024-01-03 16:50:22 -0300

    cadastrando tarfeas

--- todolist/src/components/TaskForm.tsx
+++ todolist/src/components/TaskForm.tsx
@@ -5,7 +5,6 @@ import styles from "./TaskForm.module.css";
 
 //interface
 import { ITask } from '../interfaces/Task';
-import { useState } from "react";
 
 
 type Props = {
@@ -16,14 +15,28 @@ type Props = {
 const TaskForm = ({ btnText }: Props) => {
 
   const [id, setId] = useState<number>(0);  
-  const [title, setTittle] = useState<string>("");  
+  const [title, setTitle] = useState<string>("");  
   const [difficulty, setDifficulty] = useState<number>(0);
 
+  const addTaskHandler = () => {
+
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "title") {
+        setTitle(e.target.value);
+    } else {
+        setDifficulty(parseInt(e.target.value));
+    }
+    console.log(title);
+    console.log(difficulty);
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={addTaskHandler} className={styles.form}>
         <div className={styles.input_container}>
             <label htmlFor="title">Título:</label>
-            <input type="text" name='title' placeholder='Título da tarefa' />
+            <input type="text" name='title' placeholder='Título da tarefa' onChange={handleChange}/>
         </div>
         <div className={styles.input_container}>
             <label htmlFor="difficulty">Dificuldade:</label>
