b7030640cb406d104dd600bffe5625330f588a7f
Jonatas da  Mata <jonatasdamata1@hotmail.com>
2024-01-03 09:56:16 -0300

    att

---
+++ todolist/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+     
+      <header>
+        <h1>React + TS TodoList</h1>
+      </header>
+      <h1>Conteúdo...</h1>
+      <footer>
+        <p>
+          <span>React + TS TodoList</span> @2024
+        </p>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
